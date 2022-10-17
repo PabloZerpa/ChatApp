439c93d2b86b6d6332f90c9444592014dc027e4a
Pablo <prueba@test.com>
2022-10-17 15:56:42 -0400

    Only server

--- public/src/utils/APIRoutes.js
+++
@@ -1,8 +0,0 @@
-export const host = "https://chat-app-wxyz.herokuapp.com/";
-export const registerRoute = `${host}/api/auth/register`;
-export const loginRoute = `${host}/api/auth/login`;
-export const logoutRoute = `${host}/api/auth/logout`;
-export const setAvatarRoute = `${host}/api/auth/setavatar`;
-export const allUsersRoute = `${host}/api/auth/allusers`;
-export const sendMessageRoute = `${host}/api/messages/addmsg`;
-export const recieveMessageRoute = `${host}/api/messages/getmsg`;
