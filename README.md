efe5af8da509f32393d7f3b06f9018c2d06d577f
Pablo <prueba@test.com>
2022-10-17 16:21:54 -0400

    frontend

--- README.md
+++
@@ -1,5 +0,0 @@
-# ChatApp
-## Url: https://chat-app-wxyz.netlify.app/
-
-# Screenshot
-![](./public/public/screenshoot_chatapp.png)
--- server/routes/messages.js
+++
@@ -1,8 +0,0 @@
-const { addMsg, getAllMsg } = require("../controllers/messagesController");
-
-const router = require("express").Router();
-
-router.post("/addmsg", addMsg);
-router.post("/getmsg", getAllMsg);
-
-module.exports = router;
